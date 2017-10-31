14aa0d067f8ef57ecd1ba76c62adb88f4d104372
3dchristopher <crlittleman1@yahoo.com>
2017-10-31 00:23:46 -0500

    Added Update_BAC and edited Calculate_BAC to be more concise.

--- BAC_Tracker/BAC_Tracker_Standard/Controller/BAC_Controller.cs
+++ BAC_Tracker/BAC_Tracker_Standard/Controller/BAC_Controller.cs
@@ -7,25 +7,26 @@ namespace BAC_Tracker.Controller
     public class BAC_Controller
     {
         public Model.Person Person { get; set; }
-        //public Model.Beverage Beverage {get; set; }
+        public Model.Beverage Beverage {get; set; }
         
         // List of events
         // 
 
         BAC_Controller()
         {
-            //Person = new Model.Person();
+            Person = new Model.Person();
+            Beverage = new Model.Beverage();
         }
 
-        public void Business_Logic()
+        public void Calculate_BAC(double existingBAC)
         {
-            /*int genderRate;
-            if(Person.Gender=="Female"){
-                genderRate = 1.13;
-            }else{
-                genderRate=1;
-            }
-            return ((((Beverage.completed_content*7.156655998)/weight)/100)*genderRate)+existingBAC-((time/15)*.003);*/
+            int genderRate=1;
+            if(Person.Gender=="Female") genderRate = 1.13;
+            return ((((Beverage.Completed_percentage*7.156655998)/Person.weight)/100)*genderRate)+existingBAC-((time/15)*.003);
+        }
+
+        public void Update_BAC(double existingBAC){
+            return existingBAC-(0.003);
         }
     }
 }
