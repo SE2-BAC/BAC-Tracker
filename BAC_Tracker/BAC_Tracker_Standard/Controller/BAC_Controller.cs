5413eb29e62d7371d034a36e46e5ddf15e25f136
3dchristopher <crlittleman1@yahoo.com>
2017-11-28 09:09:05 -0600

    Fixed calculate_bac function call

--- BAC_Tracker/BAC_Tracker_Standard/Controller/BAC_Controller.cs
+++ BAC_Tracker/BAC_Tracker_Standard/Controller/BAC_Controller.cs
@@ -22,12 +22,12 @@ namespace BAC_Tracker.Controller
             //Beverage = new Model.Beverage();
         }
         //NM: Void -> double since having a return value. Consideration, can pass a ref argument and do no return there.
-        public double Calculate_BAC(double existingBAC)
+        public double Calculate_BAC(Beverage Bev1, Person Per1)
         {
             //NM: Changed int -> double to handle female and return
             //NM: genderRate has male as default
             double genderRate = 1 ;
-            if (!Person.IsMale)
+            if (!Per1.IsMale)
                 //NM: Added brackets. easier to read and predictable results
             {
                 genderRate = 1.13;
@@ -36,10 +36,10 @@ namespace BAC_Tracker.Controller
             //NM: What is this time variable?
 
             DateTime currentTime = DateTime.Now;
-            TimeSpan timeT = Beverage.StartTime - currentTime;
+            TimeSpan timeT = Bev1.StartTime - currentTime;
             double timeTotal = timeT.TotalMinutes;
             
-            var BAC = (7.15665*Beverage.Alcohol_percentage*genderRate*Beverage.Percentage_consumed)/Person.Weight;
+            var BAC = (7.15665*Bev1.Alcohol_percentage*genderRate*Bev1.Percentage_consumed)/Per1.Weight;
             //Above BAC is instantaneous, the below code accounts for initial alcohol absorbtion and decay over time
              if(timeTotal<120){ //timeTotal is time since consumed in minutes
                BAC = ((BAC/120)*timeTotal)-(timeTotal*.0002);
